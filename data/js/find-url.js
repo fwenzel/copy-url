59922b9419bdf07a66cd606622bc963e69aa7c53
Fred Wenzel <fwenzel@mozilla.com>
2015-11-15 14:37:00 -0800

    Choose og:url if no canonical URL present. Fix #7.

--- data/js/find-url.js
+++ data/js/find-url.js
@@ -3,11 +3,12 @@ self.port.on('click', findUrl); // Click event from addons bar tab worker.
 
 function findUrl(node, data) {
     let url,
-        canonical;
+        canonical,
+        sel = "link[rel=canonical],meta[property='og:url']";
 
     // use canonical URL if it exists, current URL otherwise.
-    canonical = document.querySelector("link[rel=canonical]");
-    if (!(canonical && (url = canonical.href)))
+    canonical = document.querySelector(sel);
+    if (!(canonical && (url = canonical.href || canonical.content)))
         url = document.location.href;
 
     // return url with leading and trailing whitespace removed.
