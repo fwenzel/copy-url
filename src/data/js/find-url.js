2c9b82e9e108f1ff01592e74ab07670660016e3e
Fred Wenzel <code@wenzel.io>
2016-08-27 17:28:58 -0700

    reindent JS

--- src/data/js/find-url.js
+++ src/data/js/find-url.js
@@ -2,15 +2,15 @@ self.on('click', findUrl); // Context menu built-in click event.
 self.port.on('click', findUrl); // Click event from addons bar tab worker.
 
 function findUrl(node, data) {
-    let url,
-        canonical,
-        sel = "link[rel=canonical],meta[property='og:url'],link[rel=permalink]";
+  let url,
+  canonical,
+  sel = "link[rel=canonical],meta[property='og:url'],link[rel=permalink]";
 
     // use canonical URL if it exists, current URL otherwise.
     canonical = document.querySelector(sel);
     if (!(canonical && (url = canonical.href || canonical.content)))
-        url = document.location.href;
+      url = document.location.href;
 
     // return url with leading and trailing whitespace removed.
     self.postMessage(url.trim());
-}
+  }
--- src/lib/main.js
+++ src/lib/main.js
@@ -1,54 +1,54 @@
-const clipboard = require("sdk/clipboard"),
-      cm = require("sdk/context-menu"),
-      data = require("sdk/self").data,
-      notifications = require("sdk/notifications"),
-      tabs = require("sdk/tabs"),
-      ui = require("sdk/ui");
+const clipboard = require('sdk/clipboard'),
+cm = require('sdk/context-menu'),
+data = require('sdk/self').data,
+notifications = require('sdk/notifications'),
+tabs = require('sdk/tabs'),
+ui = require('sdk/ui');
 
 const addon_name = 'Copy URL',
-      addon_icon = data.url('img/world_link.png'),
-      addon_icon32 = data.url('img/world_link32.png');
+addon_icon = data.url('img/world_link.png'),
+addon_icon32 = data.url('img/world_link32.png');
 
 
 exports.main = function(options, callbacks) {
-    // Add a one-click option to toolbar.
-    var btn = ui.ActionButton({
-        id: "copy-url",
-        label: "Copy URL",
-        icon: data.url('img/world_link.png'),
-        onClick: function() {
-            var tabworker = tabs.activeTab.attach({
-                contentScriptFile: data.url('js/find-url.js'),
-                onMessage: processUrl
-            });
-            tabworker.port.emit('click');
-        },
-    })
-
-    // Add and hook up context menu
-    var ctxMenu = cm.Item({
-        label: addon_name,
-        image: addon_icon,
-        context: cm.PageContext(),
+  // Add a one-click option to toolbar.
+  var btn = ui.ActionButton({
+    id: 'copy-url',
+    label: 'Copy URL',
+    icon: data.url('img/world_link.png'),
+    onClick: function() {
+      var tabworker = tabs.activeTab.attach({
         contentScriptFile: data.url('js/find-url.js'),
         onMessage: processUrl
-    });
+      });
+      tabworker.port.emit('click');
+    },
+  })
+
+  // Add and hook up context menu
+  var ctxMenu = cm.Item({
+    label: addon_name,
+    image: addon_icon,
+    context: cm.PageContext(),
+    contentScriptFile: data.url('js/find-url.js'),
+    onMessage: processUrl
+  });
 }
 
 function notify(txt) {
-    notifications.notify({
-        title: addon_name,
-        text: txt,
-        iconURL: addon_icon32
-    })
+  notifications.notify({
+    title: addon_name,
+    text: txt,
+    iconURL: addon_icon32
+  })
 }
 
 /** Take posted URL and copy to clipboard. */
 function processUrl(url) {
-    if (url) {
-        clipboard.set(url);
-        notify("Copied URL to clipboard:\n" + url);
-    } else {
-        notify('ZOMG, error finding URL.');
-    }
+  if (url) {
+    clipboard.set(url);
+    notify('Copied URL to clipboard:\n' + url);
+  } else {
+    notify('ZOMG, error finding URL.');
+  }
 }
