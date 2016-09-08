488b0519d6ccc64495972a59f6599c64cef199e6
Fred Wenzel <code@wenzel.io>
2016-09-07 21:57:54 -0700

    Give rel=canonical precedence over og:url. Fix #16.

--- src/data/js/find-url.js
+++ src/data/js/find-url.js
@@ -2,19 +2,25 @@ self.on('click', findUrl); // Context menu built-in click event.
 self.port.on('click', findUrl); // Click event from addons bar tab worker.
 
 function findUrl(node, data) {
-  let url,
-  canonical,
-  sel = "link[rel=canonical],meta[property='og:url'],link[rel=permalink]";
+  let url;
+  let canonical;
+  const selectors = ["link[rel=canonical]", "link[rel=permalink]", "meta[property='og:url']"];
 
-    // use canonical URL if it exists, current URL otherwise.
-    canonical = document.querySelector(sel);
-    if (!(canonical && (url = canonical.href || canonical.content)))
-      url = window.location.href;
-
-    // Return found URL, fallback URL and hash to add-on for processing.
-    self.postMessage({
-      'url': url.trim(),
-      'winLoc': window.location.href,
-      'hash': window.location.hash
-    });
+  // Use canonical URL if it exists, current URL otherwise.
+  for (let i=0; i<selectors.length; i++) {
+    if (canonical = document.querySelector(selectors[i])) {
+      url = canonical.href || canonical.content;
+      break;
+    }
   }
+  if (!url) {
+    url = window.location.href;
+  }
+
+  // Return found URL, fallback URL and hash to add-on for processing.
+  self.postMessage({
+    'url': url.trim(),
+    'winLoc': window.location.href,
+    'hash': window.location.hash
+  });
+}
